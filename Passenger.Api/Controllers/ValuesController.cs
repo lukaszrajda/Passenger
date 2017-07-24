74b6661fc90a5b3967870f23f2a09b2aa509c630
lrajda <lukaszrajda@gmail.com>
2017-07-24 17:37:53 +0200

    Created default projects resolve #1

--- Passenger.Api/Controllers/ValuesController.cs
+++ Passenger.Api/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace Passenger.Api.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return $"value {id.ToString()}";
         }
 
         // POST api/values
